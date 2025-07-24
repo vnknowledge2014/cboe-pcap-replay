b87278122170cf9e13b54db437e12603d8403f4e
architectureman <vnknowledge2014@gmail.com>
2025-07-24 09:50:39 +0700

    Fix error type zero_copy_transport.rs

--- cboe-pcap-receiver/src/high_performance/zero_copy_transport.rs
+++ cboe-pcap-receiver/src/high_performance/zero_copy_transport.rs
@@ -130,10 +130,7 @@ impl ZeroCopyTransport {
                     msg_name: &self.target_addr as *const _ as *mut libc::c_void,
                     msg_namelen: std::mem::size_of::<SocketAddr>() as u32,
                     msg_iov: iovecs.as_mut_ptr(),
-                    #[cfg(target_os = "linux")]
-                    msg_iovlen: iovecs.len() as libc::c_uint,
-                    #[cfg(not(target_os = "linux"))]
-                    msg_iovlen: iovecs.len() as i32,
+                    msg_iovlen: iovecs.len(),  // Sử dụng kiểu mặc định của trường này
                     msg_control: std::ptr::null_mut(),
                     msg_controllen: 0,
                     msg_flags: 0,
